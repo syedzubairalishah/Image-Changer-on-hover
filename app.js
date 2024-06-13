a7690c87f47ace0ed03f9ed7958701ab0d71cd96
Syed Zubair Ali Shah <154551822+syedzubairalishah@users.noreply.github.com>
2024-06-13 11:16:57 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+function changeimage (x,image){
+    if(x==1){
+        image.src = 'first.jpg';
+    }
+    if(x==2){
+        image.src = 'second.jpg';
+    }
+    if(x==3){
+        image.src = 'third.jpg';
+    }
+    if(x==4){
+        image.src = 'fourth.jpg';
+    }
+    if(x==5){
+        image.src = 'fifth.jpg';
+    }
+    if(x==6){
+        image.src = 'sixth.jpg';
+    }
+    if(x==7){
+        image.src = 'seventh.jpg';
+    }
+    if(x==8){
+        image.src = 'eight.jpg';
+    }
+    if(x==9){
+        image.src = 'nine.jpg';
+    }
+    if(x==10){
+        image.src = 'ten.jpg';
+    }
+}
